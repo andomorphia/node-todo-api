7a7af4a10351e5b54a78e1de5f1acc0d0d02b5ec
Jonathan Berthier <andomorphia@gmail.com>
2018-10-20 20:02:59 +0200

    Added queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,44 @@
+const { ObjectID } = require('mongodb');
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user');
+
+// const id = '5bcb57ef96acb146dc9e6eb011';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log('Id not valid');
+// }
+
+// Todo.find({
+//   _id: id,
+// }).then(todos => {
+//   console.log('Todos', JSON.stringify(todos, undefined, 2));
+// });
+
+// Todo.findOne({
+//   _id: id,
+// }).then(todo => {
+//   console.log('Todo', JSON.stringify(todo, undefined, 2));
+// });
+
+// Todo.findById(id)
+//   .then(todo => {
+//     if (!todo) {
+//       return console.log('Id not found');
+//     }
+
+//     console.log('Todo by Id', JSON.stringify(todo, undefined, 2));
+//   })
+//   .catch(err => console.log(err));
+
+const id = '5bcb32410ea46c0b8c9946c711';
+
+User.findById(id)
+  .then(user => {
+    if (!user) {
+      return console.log('Id not found');
+    }
+
+    console.log('User by Id', JSON.stringify(user, undefined, 2));
+  })
+  .catch(err => console.log(err));
