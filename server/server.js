4a9a09b85bdbb7f54736066e611596b21ed57c1f
Jonathan Berthier <andomorphia@gmail.com>
2018-10-20 22:51:45 +0200

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
@@ -31,6 +32,27 @@ app.get('/todos', (req, res) => {
     },
     err => {
       res.status(400).send(err);
+    }
+  );
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then(
+    todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    },
+    err => {
+      res.status(400).send();
     }
   );
 });
