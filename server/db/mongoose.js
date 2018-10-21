9c7a10aabafc80298949546c6da103b6edd79780
Jonathan Berthier <andomorphia@gmail.com>
2018-10-21 16:24:01 +0200

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
+  process.env.MONGODB_URI,
   { useNewUrlParser: true }
 );
 
